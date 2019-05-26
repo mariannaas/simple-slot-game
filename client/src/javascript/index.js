cbd0db9b9d7ba8db70a88716ca42a8d69c5c4ce9
Marianna Samylova <marianna.samylova@netent.com>
2019-05-26 23:13:58 +0200

    Scaling canvas, disable spin button

--- client/src/javascript/index.js
+++ client/src/javascript/index.js
@@ -1,16 +1,8 @@
 const notificationBarElement = getElementById("notificationBar");
+let firstImageWasLoaded = false;
 
 function getElementById(element) {
     return document.getElementById(element);
-}
-
-function drawImage(context, canvas, sourceImage, index) {
-    let img = new Image();
-    img.onload = () => {
-        let newImgWidth = canvas.width / 3;
-        context.drawImage(img, index * newImgWidth, 0, newImgWidth, canvas.height);
-    };
-    img.src = sourceImage;
 }
 
 function loadImages(context, canvas, randomNumbers) {
@@ -21,8 +13,28 @@ function loadImages(context, canvas, randomNumbers) {
     }
 }
 
-function resize(canvas) {
-    let wrapperElement = document.getElementById("wrapper");
+function drawImage(context, canvas, sourceImage, index) {
+    let img = new Image();
+    img.onload = () => {
+        let newImgWidth = canvas.width / 3;
+        let newImgHeight = newImgWidth * (img.height / img.width);
+
+        if (firstImageWasLoaded === false) {
+            setCanvasHeight(canvas, newImgHeight);
+        }
+        firstImageWasLoaded = true;
+        context.drawImage(img, index * newImgWidth, 0, newImgWidth, newImgHeight);
+
+    };
+    img.src = sourceImage;
+}
+
+function setCanvasHeight(canvas, newImgHeight) {
+    canvas.height = newImgHeight;
+}
+
+function resizeCanvas(canvas) {
+    const wrapperElement = getElementById("wrapper");
     canvas.width = wrapperElement.clientWidth;
     canvas.height = wrapperElement.clientHeight;
 }
@@ -37,7 +49,7 @@ function getOutcome() {
     const context = canvas.getContext("2d");
 
     const spinOutcome = () => new Promise((resolve, reject) => {
-        let xhttp = new XMLHttpRequest(),
+        let xhttp = new XMLHttpRequest(), //create request to server
             method = "GET",
             url = "http://127.0.0.1:3000/getOutcome";
 
@@ -47,10 +59,8 @@ function getOutcome() {
                 if (this.status === 200) {
                     setTimeout(function () {
                         const response = JSON.parse(xhttp.responseText);
-                        console.log("Spin response", response);
                         loadImages(context, canvas, response.outcome);
                         if (response.isBonus) {
-                            console.log("Bonus Spin");
                             showSpinOutcome(response.winType + "\n Bonus Spin!");
                             resolve(spinOutcome());
                         } else {
@@ -64,22 +74,39 @@ function getOutcome() {
                 }
             }
         };
-        xhttp.send();
+        xhttp.send(); // send request to server
     });
-
     return spinOutcome();
+}
+
+function toggleButtonState(spinButton) {
+    let disabledStatus,
+        filterValue;
+
+    if (spinButton.disabled === true) {
+        disabledStatus = false;
+        filterValue = "none";
+    } else {
+        disabledStatus = true;
+        filterValue = "grayscale(100%)";
+    }
+    spinButton.disabled = disabledStatus;
+    spinButton.style.filter = filterValue;
 }
 
 function initAnimation() {
     document.addEventListener("DOMContentLoaded", (event) => {
         const canvas = getElementById("gameCanvas");
         const context = canvas.getContext("2d");
-        if (window.innerWidth < canvas.width) {
-            resize(canvas);
-        }
+         if (window.innerWidth < canvas.width) {
+             resizeCanvas(canvas);
+         }
         loadImages(context, canvas, [0, 1, 0]);
-        document.getElementById("spinButton").addEventListener("click", async (event) => {
+        const spinButton = getElementById("spinButton");
+        spinButton.addEventListener("click", async (event) => {
+            toggleButtonState(spinButton);
             getOutcome().then(response => {
+                toggleButtonState(spinButton);
                 console.log("Final response after resolving", response);
             }).catch(error => {
                 console.log(error);
