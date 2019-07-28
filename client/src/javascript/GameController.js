c0ef1e5f8c233c66a27a960867116d0d0c104814
Marianna Samylova <marianna.samylova@netent.com>
2019-07-28 10:03:06 +0200

    GameModel getOutcome method was added

--- client/src/javascript/GameController.js
+++ client/src/javascript/GameController.js
@@ -6,30 +6,9 @@ export default class GameController {
         this.gameModel = new GameModel();
     }
 
-    getOutcome() {
-        const spinOutcome = () => new Promise((resolve, reject) => {
-            let xhttp = new XMLHttpRequest(), //create request to server
-                method = "GET",
-                url = "http://127.0.0.1:3000/getOutcome";
-
-            xhttp.open(method, url, true);
-            xhttp.onreadystatechange = function () {
-                if (this.readyState === 4) {
-                    if (this.status === 200) {
-                        const response = JSON.parse(xhttp.responseText);
-                        resolve(response);
-                    } else {
-                        reject("Server is currently unavailable. Please reload the game or try it later!");
-                    }
-                }
-            };
-            xhttp.send(); // send request to server
-        });
-        return spinOutcome();
-    }
 
     processOutcome() {
-        this.getOutcome().then(response => {
+        this.gameModel.getOutcome().then(response => {
             console.log('Got response: ', response);
 
             setTimeout(function () {
--- client/src/javascript/GameModel.js
+++ client/src/javascript/GameModel.js
@@ -2,5 +2,26 @@ export default class GameModel {
     constructor() {
 
     }
+    getOutcome() {
+        const spinOutcome = () => new Promise((resolve, reject) => {
+            let xhttp = new XMLHttpRequest(), //create request to server
+                method = "GET",
+                url = "http://127.0.0.1:3000/getOutcome";
+
+            xhttp.open(method, url, true);
+            xhttp.onreadystatechange = function () {
+                if (this.readyState === 4) {
+                    if (this.status === 200) {
+                        const response = JSON.parse(xhttp.responseText);
+                        resolve(response);
+                    } else {
+                        reject("Server is currently unavailable. Please reload the game or try it later!");
+                    }
+                }
+            };
+            xhttp.send(); // send request to server
+        });
+        return spinOutcome();
+    }
 
 }
